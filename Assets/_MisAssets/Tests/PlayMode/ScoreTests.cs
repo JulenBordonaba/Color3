0017e30f1cc61257c493fb299e21f1bec301e8f5
roboioritz <48485367+roboioritz@users.noreply.github.com>
2020-12-14 18:04:44 +0100

    Creada carpeta tests y test del score manager
    CReada carpeta tests y subcarpeta playmode
    creado el asembly de scripts
    Creado el script ScoreTests
    Testeada clase ScoreManager

---
+++ Assets/_MisAssets/Tests/PlayMode/ScoreTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace Tests
+{
+    public class ScoreTests
+    {        
+        [UnityTest]
+        public IEnumerator ScoreTestsWithEnumeratorPasses()
+        {
+            GameObject go = new GameObject();
+            ScoreManager sc = go.AddComponent<ScoreManager>();
+            sc.scoreText = go.AddComponent<Text>();
+
+            yield return new WaitForSeconds(1);
+
+            Assert.AreEqual(0, sc.score);
+            Assert.AreEqual("0", sc.scoreText.text);
+
+            sc.AddScore();
+            yield return new WaitForSeconds(1);
+
+            Assert.AreEqual(1, sc.score);
+            Assert.AreEqual("1", sc.scoreText.text);
+
+            for (int i = 0; i < 9998; i++) sc.AddScore();
+            yield return new WaitForSeconds(1);
+
+            Assert.AreEqual(9999, sc.score);
+            Assert.AreEqual("9999", sc.scoreText.text);
+
+            sc.ResetScore();
+            yield return new WaitForSeconds(1);
+
+            Assert.AreEqual(0, sc.score);
+            Assert.AreEqual("0", sc.scoreText.text);
+            yield return null;
+        }
+    }
+}
